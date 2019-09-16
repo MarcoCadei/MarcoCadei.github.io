9be785db144bd373f34bdc493464d0f23af14fd1
Marco <m.cadei010@studenti.unibs.it>
2019-09-16 18:15:41 +0200

    Updates

---
+++ precache-manifest.be89db2fcc6399e72407ed166e254cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1c410c8bebe021a4d0396858de2c03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d64d62699aa424e01526",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ecf723206d47f1bcedbf",
+    "url": "/static/js/2.dcaa7138.chunk.js"
+  },
+  {
+    "revision": "d64d62699aa424e01526",
+    "url": "/static/js/main.ec55e55f.chunk.js"
+  },
+  {
+    "revision": "fd9401c5a4cc0adeb619",
+    "url": "/static/js/runtime~main.a45aa8df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
