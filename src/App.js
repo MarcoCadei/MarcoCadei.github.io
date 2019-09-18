5142394e4dce43cc12986e418352539824d7729a
Marco <m.cadei010@studenti.unibs.it>
2019-09-18 19:06:25 +0200

    Aggiunge versione iniziale della landing page con scritte

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import Fade from '@material-ui/core/Fade';
+import Slide from '@material-ui/core/Slide';
+import Box from '@material-ui/core/Box';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount(){
+    this.setState({loaded: true});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <Fade in={this.state.loaded} timeout={2000}>
+            <div>
+              <Box fontWeight="fontWeightBold" fontSize={100} fontFamily="Monospace">Marco Cadei</Box>
+              <Box fontSize={15} fontFamily="Monospace">
+                Computer Science & Engineering Master's Student @ University of Brescia
+              </Box>
+              <Box fontSize={15} fontFamily="Monospace">
+                Software Engineer Intern @ Zucchetti
+              </Box>
+            </div>
+          </Fade>
+          
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
