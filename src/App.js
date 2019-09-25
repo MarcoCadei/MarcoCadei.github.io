f1b8b6cadacd9fb7d8b981ab20ea7a7b6deafb45
Marco <m.cadei010@studenti.unibs.it>
2019-09-25 17:21:09 +0200

    Completa pagina iniziale

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+import { Typography } from '@material-ui/core';
 import Fade from '@material-ui/core/Fade';
-import Slide from '@material-ui/core/Slide';
-import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import DescriptionIcon from '@material-ui/icons/Description';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import Link from '@material-ui/core/Link';
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
@@ -26,19 +29,32 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          
-          <Fade in={this.state.loaded} timeout={2000}>
-            <div>
-              <Box fontWeight="fontWeightBold" fontSize={100} fontFamily="Monospace">Marco Cadei</Box>
-              <Box fontSize={15} fontFamily="Monospace">
-                Computer Science & Engineering Master's Student @ University of Brescia
-              </Box>
-              <Box fontSize={15} fontFamily="Monospace">
-                Software Engineer Intern @ Zucchetti
-              </Box>
-            </div>
-          </Fade>
-          
+            <Fade in={this.state.loaded} timeout={2000}>
+              <div>
+                <ThemeProvider theme={theme}>
+                  <Typography variant="h1">Marco Cadei</Typography>
+                  <Typography variant="body2">
+                    Computer Science & Engineering Master's Student @ University of Brescia
+                  </Typography>
+                  <Typography variant="body2">
+                    Software Engineer Intern @ Zucchetti
+                  </Typography>
+                  <IconButton color="primary" size="medium" style={{color: "white"}} aria-label="Resume (CV)">
+                    <DescriptionIcon fontSize="large" />
+                  </IconButton>
+                  <IconButton color="primary" size="medium" style={{color: "white"}} aria-label="Resume (CV)">
+                    <Link href="https://www.linkedin.com/in/marcocadei/" color="inherit" target="_blank">
+                      <LinkedInIcon fontSize="large" />
+                    </Link>
+                  </IconButton>
+                  <IconButton color="primary" size="medium" style={{color: "white"}} aria-label="Resume (CV)">
+                    <Link href="https://www.instagram.com/marco_cadei/" color="inherit" target="_blank">
+                      <InstagramIcon fontSize="large" />
+                    </Link>
+                  </IconButton>
+                </ThemeProvider>
+              </div>
+            </Fade>
         </header>
       </div>
     );
